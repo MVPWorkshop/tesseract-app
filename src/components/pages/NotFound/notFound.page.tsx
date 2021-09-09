ef1a740c5f6c3655197d11defa8fc6e2e35dde9d
Filip Pajic <fica.pajic@gmail.com>
2021-09-09 11:18:57 +0200

    Added 404 illustration

--- src/components/pages/NotFound/notFound.page.tsx
+++ src/components/pages/NotFound/notFound.page.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import PageOrganism from "../../organisms/Page/page.organism";
 import { EHeaderType } from "../../organisms/Header/header.organism.types";
-import { ReactComponent as NotFoundIllustrationSVG } from "../../../shared/assets/tesseract-logo.svg";
+import { ReactComponent as NotFoundIllustrationSVG } from "../../../shared/assets/404.svg";
+import Typography from "../../atoms/Typography/typography.atom";
+import { Trans } from "@lingui/macro";
+import { EFontWeight } from "../../../shared/types/styles.types";
+import styles from "./notFound.page.module.scss";
 
 const NotFoundPage: React.FC = () => {
   return (
     <PageOrganism
       headerType={EHeaderType.LANDING}
-      containerEnabled={true}
-      className="d-flex align-items-center justify-content-center"
+      className={styles.notFoundPage}
     >
-      <NotFoundIllustrationSVG/>
+      <NotFoundIllustrationSVG className={styles.illustration} />
+      <br/>
+      <Typography
+        uppercase={true}
+        fontWeight={EFontWeight.SEMI_BOLD}
+        fontSize={20}
+      >
+        <Trans> Page not found </Trans>
+      </Typography>
     </PageOrganism>
   );
 };
