dd6640925ced5040dccf7ee23070fbf128660d4e
Gerard Arguelles <gerard.arguelles@gmail.com>
2022-03-10 15:34:13 +0800

    connect wallet

--- src/components/organisms/WalletList/walletList.organism.tsx
+++ src/components/organisms/WalletList/walletList.organism.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useDispatch } from "react-redux";
 import Typography from "../../atoms/Typography/typography.atom";
 import { CONNECTOR_LABELS, CONNECTOR_LOGOS, CONNECTOR_DESCRIPTIONS, supportedConnectorList } from "../../../shared/constants/web3.constants";
 import { EColor, EFontWeight } from "../../../shared/types/styles.types";
 import {EConnectorType} from "../../../shared/types/web3.types";
 import styles from "./walletList.organism.module.scss";
+import WalletService from "../../../shared/services/wallet/wallet.service";
+import useWeb3 from "../../../hooks/useWeb3";
+import {EModalName} from "../../../redux/ui/ui.redux.types";
+import {toggleModal} from "../../../redux/ui/ui.redux.actions";
 
 const WalletList: React.FC = () => {
-  const test = () => {
-    return true;
+  const dispatch = useDispatch();
+  const { activate, error, connector, active, account, chainId, mappedError } = useWeb3();
+
+  const connectWalletFnFactory = (type_: EConnectorType) => {
+    return () => {
+      const _connector = WalletService.typeToProvider(type_);
+      activate(_connector).finally(() => dispatch(toggleModal(EModalName.CONNECT_WALLET_V2, false)));
+    };
   };
+
+  useEffect(() => {
+    if (active) {
+      dispatch(toggleModal(EModalName.CONNECT_WALLET_V2, false));
+    }
+  }, [active])
 
   const renderSupportedWallets = () => (
     supportedConnectorList.map((type_: EConnectorType) => {
       const Logo = CONNECTOR_LOGOS[type_];
 
       return (
-        <li key={type_} onClick={test} className={styles.walletItem}>
+        <li 
+          key={type_} 
+          onClick={connectWalletFnFactory(type_)} 
+          className={styles.walletItem}
+        >
           <Logo className={styles.walletLogo}/>
           <Typography 
             color={EColor.BLACK} 
