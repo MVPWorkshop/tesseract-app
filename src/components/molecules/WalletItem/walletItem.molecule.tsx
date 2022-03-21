d4f760261686ed5ceb69362ac9b79b4ff7ec2580
Gerard Arguelles <gerard.arguelles@gmail.com>
2022-03-21 16:32:36 +0800

    [fix] wallet list keys

--- src/components/molecules/WalletItem/walletItem.molecule.tsx
+++ src/components/molecules/WalletItem/walletItem.molecule.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import {CONNECTOR_METADATA} from "../../../shared/constants/web3.constants";
-import {EColor, EFontWeight} from "../../../shared/types/styles.types";
+import { CONNECTOR_METADATA } from "../../../shared/constants/web3.constants";
+import { EColor, EFontWeight } from "../../../shared/types/styles.types";
 import Typography from "../../atoms/Typography/typography.atom";
-import {IWalletItem} from "./walletItem.molecule.types";
+import { IWalletItem } from "./walletItem.molecule.types";
 import styles from "./walletItem.molecule.module.scss";
 
 const WalletItem: React.FC<IWalletItem> = (props) => {
-  const {connectorType, onClick} = props;
-  const {label, description, logo: Logo} = CONNECTOR_METADATA[connectorType];
+  const { connectorType, onClick } = props;
+  const { label, description, logo: Logo } = CONNECTOR_METADATA[connectorType];
 
   return (
-    <li 
-      key={connectorType} 
-      onClick={onClick} 
+    <li
+      key={connectorType}
+      onClick={onClick}
       className={styles.walletItem}
     >
-      <Logo className={styles.walletLogo}/>
-      <Typography 
-        color={EColor.BLACK} 
-        fontSize={24} 
+      <Logo className={styles.walletLogo} />
+      <Typography
+        color={EColor.BLACK}
+        fontSize={24}
         fontWeight={EFontWeight.BOLD}
       >
-        {label} 
+        {label}
       </Typography>
       <Typography
         color={EColor.DARK_GREY}
         fontSize={18}
       >
-        {description} 
-      </Typography> 
+        {description}
+      </Typography>
     </li>
   );
 };
