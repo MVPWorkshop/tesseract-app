61511f408252c024332627afef48861a84aad2d6
Gerard Arguelles <gerard.arguelles@gmail.com>
2022-03-24 15:11:05 +0800

    vault header styles

--- src/components/molecules/VaultHeader/vaultHeader.molecule.tsx
+++ src/components/molecules/VaultHeader/vaultHeader.molecule.tsx
@@ -3,30 +3,30 @@ import {tokenIcons} from "../../../shared/constants/common.constants";
 import {ESupportedTokens} from "../../../shared/types/vault.types";
 import InfoBox from "../../atoms/InfoBox/infoBox.atom";
 import TokenDetail from "../../atoms/TokenDetail/tokenDetail.atom";
-import {AssetType} from "../../atoms/TokenDetail/tokenDetail.atom.types";
+import {EAssetType} from "../../atoms/TokenDetail/tokenDetail.atom.types";
 import styles from "./vaultHeader.molecule.module.scss";
 
 const VaultHeader: React.FC = () => {
   return (
     <div className={styles.vaultHeader}>
-      <div style={{ flexGrow: 2 }}>
+      <div className={styles.tokenInfoColumn}>
         <TokenDetail
-          assetType={AssetType.Token}
+          assetType={EAssetType.Token}
           name="Matic"
           purchaseLink="https://test.com"
           logo={tokenIcons[ESupportedTokens.WMATIC]}
         />
       </div>
-      <div style={{ flexGrow: 1 }}>
+      <div className={styles.tokenDataColumn}>
         <InfoBox usdValue="$12,000" value="2" footer="Wallet" />
       </div>
-      <div style={{ flexGrow: 1 }}>
+      <div className={styles.tokenDataColumn}>
         <InfoBox value="0" footer="Deposited" />
       </div>
-      <div style={{flexGrow: 1 }}>
+      <div className={styles.tokenDataColumn}>
         <InfoBox value="5%" footer="APY" />
       </div>
-      <div style={{ flexGrow: 1 }}>
+      <div className={styles.tokenDataColumn}>
         <InfoBox value="$5,223,562" footer="TVL" />
       </div>
     </div>
@@ -34,4 +34,3 @@ const VaultHeader: React.FC = () => {
 };
 
 export default VaultHeader;
-
