ab765a2870de7a123c630517c278b950d38cbff0
Gerard Arguelles <gerard.arguelles@gmail.com>
2022-03-24 17:28:41 +0800

    use typograhpy

--- src/components/atoms/InfoBox/infoBox.atom.tsx
+++ src/components/atoms/InfoBox/infoBox.atom.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { IInfoBox } from "./infoBox.atom.types";
 import styles from "./infoBox.atom.module.scss";
+import Typography from "../Typography/typography.atom";
+import {EColor, EFontWeight} from "../../../shared/types/styles.types";
 
 const InfoBox: React.FC<IInfoBox> = (props) => {
   const {usdValue, value, footer} = props;
 
   return (
     <div className={styles.infoBox}>
-      { usdValue && <p className={styles.label}>{usdValue}</p>}
-      <p className={styles.value}>{value}</p>
-      <p className={styles.label}>{footer}</p>
+      { usdValue && (
+        <Typography 
+          fontSize={13}
+          fontWeight={EFontWeight.SEMI_BOLD}
+          color={EColor.RHYTM}
+        >
+          {usdValue}
+        </Typography>
+      )}
+      <Typography
+        fontSize={19}
+        fontWeight={EFontWeight.BOLD}
+      >
+        {value}
+      </Typography>
+      <Typography
+        fontSize={13}
+        fontWeight={EFontWeight.SEMI_BOLD}
+        color={EColor.RHYTM}
+      >
+        {footer}
+      </Typography>
     </div>
   );
 };
