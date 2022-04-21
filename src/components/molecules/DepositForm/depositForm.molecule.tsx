d9a71e6bc2f3b70d06f88b3b58745eb284e935b8
Gerard Arguelles <gerard.arguelles@gmail.com>
2022-04-21 14:42:46 +0800

    query vault data

--- src/components/molecules/DepositForm/depositForm.molecule.tsx
+++ src/components/molecules/DepositForm/depositForm.molecule.tsx
@@ -23,7 +23,7 @@ import ActionUtil from "../../../shared/utils/action.util";
 import { ETokenReduxActions, ITokenReduxState } from "../../../redux/tokens/tokens.redux.types";
 import { createLoadingSelector } from "../../../redux/loading/loading.redux.reducer";
 import { Nullable } from "../../../shared/types/util.types";
-import { EVaultReduxActions } from "../../../redux/vaults/vaults.redux.types";
+import { EVaultReduxActions, IVaultReduxState } from "../../../redux/vaults/vaults.redux.types";
 
 
 const DepositForm: React.FC<IDepositFormProps> = (props) => {
@@ -44,6 +44,13 @@ const DepositForm: React.FC<IDepositFormProps> = (props) => {
     amountApproved
   } = useSelector<RootState, ITokenReduxState>(state => state.tokens[token]);
 
+  const vaultData: IVaultReduxState | null = useSelector<RootState, IVaultReduxState | null>(state => {
+    if (vaultAddress) {
+      return state.vaults[vaultAddress];
+    } else {
+      return null;
+    }
+  });
 
   const isFetchingApprovedTokenAmount = useSelector<RootState, boolean>(
     createLoadingSelector([ActionUtil.actionName(ETokenReduxActions.FETCH_TOKEN_APPROVED_AMOUNT, token)])
