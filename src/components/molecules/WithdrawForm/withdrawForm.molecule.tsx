6f717d1d2da6b6d1e830b5287b7f0d9556e5df67
garguelles <gerard.arguelles@gmail.com>
2022-05-13 17:37:17 +0800

    [fix] withdraw value rounding

--- src/components/molecules/WithdrawForm/withdrawForm.molecule.tsx
+++ src/components/molecules/WithdrawForm/withdrawForm.molecule.tsx
@@ -61,12 +61,14 @@ const WithdrawForm: React.FC<IVaultForm> = (props) => {
   const tokenLabel = (tokenLabels[token] && tokenLabels[token][chainId]) ? tokenLabels[token][chainId] : token;
   const sliderMarks = [1, 25, 50, 75, 100];
   const formattedUserShares = (vaultData && vaultData.userShares && vaultData.sharePrice && decimals) ? getShareInFormattedToken(vaultData.userShares, vaultData.sharePrice, decimals).round(6) : null;
+  const depositedValue = (vaultData && vaultData.userShares && vaultData.sharePrice && decimals) ? getShareInFormattedToken(vaultData.userShares, vaultData.sharePrice, decimals) : null;
+
   const isWithdrawAllAssetsDisabled = !(vaultAddress && account && chainId && !!signer);
   const isWithdrawSomeAssetsDisabled = isWithdrawAllAssetsDisabled || !(isBigDecimalGt(withdrawValue.actual, new BigDecimal(0)) && decimals && vaultData?.sharePrice);
 
 
   const renderUserShares = () => {
-    const value = (formattedUserShares || new BigDecimal(0)).round(decimals);
+    const value = (depositedValue || new BigDecimal(0)).round(decimals);
 
     const userShareValue = formattedUserShares?.getValue();
     const userShareText = `${userShareValue} ${tokenLabel}`;
@@ -93,7 +95,7 @@ const WithdrawForm: React.FC<IVaultForm> = (props) => {
 
   const onWithdrawPercentageChange = (percentage: number) => {
     const value =
-      (formattedUserShares || new BigDecimal(0))
+      (depositedValue || new BigDecimal(0))
         .multiply(new BigDecimal(percentage))
         .divide(new BigDecimal(100), 64)
         .round(decimals);
@@ -171,7 +173,7 @@ const WithdrawForm: React.FC<IVaultForm> = (props) => {
             className={styles.actionButton}
             disabled={isWithdrawSomeAssetsDisabled || isDepositingAssets || isWithdrawingAllAssets}
             loading={isWithdrawingAssets}
-            onClick={withdrawAssets(withdrawValue.percent === 100)}
+            onClick={withdrawAssets(false)}
           >
             <Trans>Withdraw</Trans>
           </Button>
